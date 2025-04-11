60397eca270866facc54d1aeaf0b79d77dcba300
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-04-11 08:17:02 +0200

    adding app images

---
+++ lib/core/constants/app_images.dart
@@ -0,0 +1,19 @@
+sealed class AppImages{
+  // logos
+  static const routeLogo = "assets/images/route_logo.png";
+
+  // images
+  static const defaultProfilePic = "assets/images/default_profile_pic.jpg";
+
+  // icons
+  static const mailIcon = "assets/icons/mail_icon.svg";
+  static const phoneIcon = "assets/icons/phone_icon.svg";
+  static const deleteIcon = "assets/icons/delete_icon.svg";
+  static const cameraIcon = "assets/icons/camera_icon.png";
+  static const galleryIcon = "assets/icons/gallery_icon.png";
+
+  // animations
+  static const emptyListAnimation = "assets/images/empty_list.json";
+  static const emptyListNewAnimation = "assets/images/empty_list_new.json";
+  static const imagePickerAnimation = "assets/images/image_picker.json";
+}
